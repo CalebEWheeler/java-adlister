e2cbcc5531cabe8e72c13f33ad29e446a4d719ad
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-11-02 17:03:22 -0600

    feat: create new java-adlist maven project that is configured with tomcat. Has a simple HelloWorldServlet.class that has a servlet that when run will open a page in the browser and tell the user a message, and if query values are set for name and grade it will display those passed values

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,21 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("Hello World!");
 
+        String name = req.getParameter("name");
+        out.printf("<h3>Hello, " + name + "</h3>");
+
+        int grade = Integer.parseInt(req.getParameter("grade"));
+        out.println("<span>Grade: " + grade + ".00</span>");
     }
 }
